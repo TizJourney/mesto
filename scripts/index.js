7ad3ac4d730608546037f23f2172f13f77a6d0f6
Konstantin Bondar <kostya.bondar@gmail.com>
2020-08-23 22:31:55 +0300

    feat: написать скрипнт для генерации динамических карточек

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,50 @@ const editProfileButtonElement = profileElement.querySelector('.profile__edit-bu
 const titleProfile = profileElement.querySelector('.profile__title');
 const descriptionProfile = profileElement.querySelector('.profile__description');
 
+
+//Генерация динамических карточек
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardTemplate = document.querySelector('#card-temlate').content;
+
+cardPreparedElements = initialCards.map(function (cardContent) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = cardContent['link'];
+  cardImage.alt = `Изображение места ${cardContent['name']}`;
+  cardElement.querySelector('.card__title').textContent = cardContent['name'];
+  return cardElement;
+});
+
+const cardContainer = document.querySelector('.elements__card-container');
+cardContainer.append(...cardPreparedElements);
+
+// функциональность "Изменить профиль"
 function showPopup() {
 
   nameInput.value = titleProfile.textContent;
