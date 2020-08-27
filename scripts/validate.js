5bf42aa6c806aa5201107e65d095279668d6602c
Konstantin Bondar <kostya.bondar@gmail.com>
2020-08-27 14:11:03 +0300

    refactor: заготовка для рефакторинга валидации попапов

--- scripts/validate.js
+++ scripts/validate.js
@@ -64,3 +64,12 @@ const enableValidation = () => {
 
 // Инициализация
 enableValidation();
+
+// enableValidation({
+//   formSelector: '.popup__form',
+//   inputSelector: '.popup__input',
+//   submitButtonSelector: '.popup__button',
+//   inactiveButtonClass: 'popup__button_disabled',
+//   inputErrorClass: 'popup__input_type_error',
+//   errorClass: 'popup__error_visible'
+// });
