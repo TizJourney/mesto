3a2fedcf8f37171f1a88080bd003d5b7c5a11ddb
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-08 20:17:57 +0300

    feat: Добавлен класс popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,44 @@
+export default class Popup {
+  constructor(popupSelector) {
+    self._popupElement = document.querySelector(popupSelector);
+  }
+
+  _handleEscClose(event) {
+    if (event.key === 'Escape') {
+      this.close();
+    }
+  }
+
+  _handleClosePopupOnClick(event) {
+    if (
+      event.target.classList.contains('popup__close-button') ||
+      event.target.classList.contains('popup__overlay')
+    ) {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    self._popupElement.addEventListener('click', this._handleEscClose.bind(this));
+    self._popupElement.addEventListener('keydown', this._handleClosePopupOnClick.bind(this));
+  }
+
+  removeEventListener() {
+    self._popupElement.removeEventListener('click', this._handleEscClose);
+    self._popupElement.removeEventListener('keydown', this._handleClosePopupOnClick);
+  }
+
+  open() {
+    self._popupElement.classList.add('popup_opened');
+    this.setEventListeners()
+  }
+
+  close() {
+    self._popupElement.classList.remove('popup_opened');
+    this.removeEventListener();
+  }
+}
+
 function hidePopupCallback(event) {
   const activePopup = document.querySelector('.popup_opened');
   if (event.key === 'Escape') {
